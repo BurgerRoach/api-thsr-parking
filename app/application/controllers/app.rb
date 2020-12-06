1b91084dbb68c892ba395dd283f72b4808ce1b18
music1353 <music1353@gmail.com>
2020-12-06 16:47:49 +0800

    fix: query error

--- app/application/controllers/app.rb
+++ app/application/controllers/app.rb
@@ -30,8 +30,6 @@ module THSRParking
       routing.on 'api/v1' do
         routing.on 'cities' do
           routing.is do
-            # GET /cities
-            # GET /cities?city_id={}
             routing.get do
               # GET /cities
               if routing.params.length.zero?
@@ -45,37 +43,25 @@ module THSRParking
                 http_response = Representer::HttpResponse.new(result.value!)
                 response.status = http_response.http_status_code
                 Representer::CitiesList.new(result.value!.message).to_json
-
+              # GET /cities?
               else
                 q_key, q_value = Request::CityQueryParser.new(routing.params).parse.value!
 
-                # GET /cities?city_name=
-                if q_key == 'city_name'
+                case q_key
+                when 'city_name' # GET /cities?city_name={name}
                   result = Service::Cities.new.find_by_name(q_value)
-
-                  if result.failure?
-                    failed = Representer::HttpResponse.new(result.failure)
-                    routing.halt failed.http_status_code, failed.to_json
-                  end
-
-                  http_response = Representer::HttpResponse.new(result.value!)
-                  response.status = http_response.http_status_code
-                  Representer::CityResult.new(result.value!.message).to_json
-                end
-
-                # GET /cities?city_id=
-                if q_key == 'city_id'
+                when 'city_id' # GET /cities?city_id=={id}
                   result = Service::Cities.new.find_by_id(q_value)
-
-                  if result.failure?
-                    failed = Representer::HttpResponse.new(result.failure)
-                    routing.halt failed.http_status_code, failed.to_json
-                  end
-
-                  http_response = Representer::HttpResponse.new(result.value!)
-                  response.status = http_response.http_status_code
-                  Representer::CityResult.new(result.value!.message).to_json
                 end
+
+                if result.failure?
+                  failed = Representer::HttpResponse.new(result.failure)
+                  routing.halt failed.http_status_code, failed.to_json
+                end
+
+                http_response = Representer::HttpResponse.new(result.value!)
+                response.status = http_response.http_status_code
+                Representer::CityResult.new(result.value!.message).to_json
               end
             end
           end
